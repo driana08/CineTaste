b011ee409cc9cb5745d0707ea9d6c360e345390b
driana08 <adrianavaldesprado08@gmail.com>
2024-09-03 22:29:46 -0400

    Só faltam tres

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.getElementById("cinema").onclick = function() {
     window.location.href = "cine.html"
 }
+document.getElementById("cafe").onclick = function() {
+    window.location.href = "cafe.html"
+}
+document.getElementById("doce").onclick = function() {
+    window.location.href = "doce.html"
+}
